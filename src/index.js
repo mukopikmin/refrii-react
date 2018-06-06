ac2a4e3e25c61d15ffd4e2e9194ee6bdb54fb694
Yusuke Mukai <opposite0302@gmail.com>
2018-06-06 23:16:57 +0900

    added eslint env config

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import createSagaMiddleware from 'redux-saga';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
-import rootSaga from './sagas';
-import AppContainer from './containers/AppContainer';
+import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 import logger from 'redux-logger';
 import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
+import AppContainer from './containers/AppContainer';
+import reducers from './reducers';
+import rootSaga from './sagas';
+import './index.css';
 // import registerServiceWorker from './registerServiceWorker';
 
 const persistConfig = {

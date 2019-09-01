69a6333b0abd63bda7db2dd442af61424b88d371
Yusuke Mukai <mukopikmin@gmail.com>
2019-09-01 18:07:56 +0900

    fix updating box

--- src/sagas/boxSaga.js
+++ src/sagas/boxSaga.js
@@ -10,10 +10,12 @@ function* handleRequestListBox() {
   try {
     const session = yield select(selectors.getSession);
     const boxes = yield call(Box.getBoxes, session.jwt);
+
     yield put(actions.receiveListBox(boxes));
 
     if (boxes.length > 0) {
       const box = yield select(selectors.getSelectedBox);
+
       if (!box) {
         yield put(actions.selectBox(boxes[0]));
       }
@@ -28,10 +30,9 @@ function* handleRequestCreateBox(action) {
   try {
     const session = yield select(selectors.getSession);
     const { box } = action.payload;
+
     yield call(Box.createBox, session.jwt, box);
     yield put(actions.receiveCreateBox());
-    // const boxes = yield call(Box.getBoxes, session.jwt);
-    // yield put(actions.receiveListBox(boxes));
     yield put(actions.showNotification(`${box.name} が作成されました`));
   } catch (error) {
     yield put(actions.failedCreateBox(error));
@@ -43,10 +44,9 @@ function* handleRequestUpdateBox(action) {
   try {
     const { box } = action.payload;
     const session = yield select(selectors.getSession);
-    yield call(Box.updateBox, session.jwt, box);
-    yield put(actions.receiveUpdateBox());
-    // const boxes = yield call(Box.getBoxes, session.jwt);
-    // yield put(actions.receiveListBox(boxes));
+    const updated = yield call(Box.updateBox, session.jwt, box);
+
+    yield put(actions.receiveUpdateBox(updated));
     yield put(actions.showNotification(`${box.name} が更新されました`));
   } catch (error) {
     yield put(actions.failedUpdateBox(error));
@@ -58,10 +58,9 @@ function* handleRequestRemoveBox(action) {
   try {
     const { box } = action.payload;
     const session = yield select(selectors.getSession);
+
     yield call(Box.removeBox, session.jwt, box.id);
     yield put(actions.receiveRemoveBox(box.id));
-    // const boxes = yield call(Box.getBoxes, session.jwt);
-    // yield put(actions.receiveListBox(boxes));
     yield put(actions.showNotification(`${box.name} が削除されました`));
   } catch (error) {
     yield put(actions.failedRemoveBox(error));
@@ -73,10 +72,9 @@ function* handleRequestInviteBox(action) {
   try {
     const { box, email } = action.payload;
     const session = yield select(selectors.getSession);
+
     yield call(Box.invite, session.jwt, box.id, email);
     yield put(actions.receiveInviteBox());
-    // const boxes = yield call(Box.getBoxes, session.jwt);
-    // yield put(actions.receiveListBox(boxes));
     yield put(actions.showNotification(`${box.name} が共有されました`));
   } catch (error) {
     yield put(actions.failedInviteBox(error));
